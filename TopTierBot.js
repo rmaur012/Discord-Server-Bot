d7b9af382867697197e77dc07515e0cdf5e131e5
rmaur012 <rmaur012@fiu.edu>
2018-01-29 15:34:09 -0500

    Got the bot connected and working. Added the tourney command that will display tourney info. Added 2 json files that will serve to hold tourey and general info.

--- TopTierBot.js
+++ TopTierBot.js
@@ -1,6 +1,9 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var tourneys = require('./tourneys.json');
+var info = require('./info.json');
+
 var quotes = ["\"You need to stop.\" -Joe", "\"Eddy, you're a FUCKING WEEB!\" -Jojo", "\"This is why I went to the math department.\" -Pablo", "\"Am I right fellas?\" -Danny", "\"Joe I'll do a $100 money match once I go 4-2, I swear to god. Eskeetit\" -Yoshi Main"];
 var quoteIndex = 0;
 
@@ -21,6 +24,17 @@ function getRNGInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function sendMessage(reply, channelID){
+    bot.sendMessage({
+        to: channelID,
+        message: reply
+    });
+}
+
+function displayTourneyInfo(place, channelID) {
+    sendMessage('Challonge: ' + place.challonge + '\nStream: ' + place.stream, channelID);
+}
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
@@ -37,49 +51,64 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         args = args.splice(1);
         switch (cmd) {
             // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
+            case 'hi':
+                sendMessage('Hello, I main Bayonetta.', channelID);
+                
                 break;
             case 'quote':
-                bot.sendMessage({
-                    to: channelID,
-                    message: quotes[quoteIndex]
-                });
+                sendMessage(quotes[quoteIndex], channelID);
                 quoteIndex = quoteIndex + 1;
                 if (quoteIndex == quotes.length) {
                     quoteIndex = 0;
                 }
                 break;
 
+            case 'tourney':
+                var type = args[0];
+                switch (type) {
+                    case 'ao':
+                    case 'AO':
+                        {
+                            displayTourneyInfo(tourneys.ao, channelID);
+                        }
+                        break;
+                    case 'vs':
+                    case 'VS':
+                        {
+                            displayTourneyInfo(tourneys.vs, channelID);
+                        }
+                        break;
+                    case 'ecg':
+                    case 'ECG':
+                        {
+                            displayTourneyInfo(tourneys.ecg, channelID);
+                        }
+                        break;
+                    default:
+                        sendMessage('You didn\'t say which tournament.',  channelID);
+                }
+
+                break;
+
             case 'roll':
                 var rollType = args[0];
-                switch (rollType):
+                switch (rollType) {
                     case '1d20':
                         var num = getRNGInteger(1, 20);
-                        var rollStr = "You rolled a " + num.toString + "!";
-                        
+                        var rollStr = "You rolled a " + num.toString() + "!";
+
                         //Add the parts that adds the modifiers to the roll
-                        
-                        bot.sendMessage({
-                            to: channelID,
-                            message: rollStr
-                        });
+
+                        sendMessage(rollStr, channelID);
+                        break;
 
                     default:
-                        bot.sendMessage({
-                            to: channelID,
-                            message: 'Just like target test in Smash 4, that command doesn\'t exist.'
-                        });
-
+                        sendMessage('Just like target test in Smash 4, that command doesn\'t exist.', channelID);
+                }
+                break;
 
             default:
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Just like target test in Smash 4, that command doesn\'t exist.'
-                });
+                sendMessage('Just like target test in Smash 4, that command doesn\'t exist.', channelID);
                 // Just add any case commands if you want to..
         }
     }
