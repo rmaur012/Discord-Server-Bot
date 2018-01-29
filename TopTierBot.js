62430566580a30dddf714e16204f81106ebca6ed
rmaur012 <rmaur012@fiu.edu>
2018-01-29 00:40:42 -0500

    Added the groundwork for the D&D rolls. Need to add parts that includes modifiers to the roll

--- TopTierBot.js
+++ TopTierBot.js
@@ -13,9 +13,13 @@ logger.level = 'debug';
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun: true
 });
+
+function getRNGInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 bot.on('ready', function (evt) {
     logger.info('Connected');
@@ -29,27 +33,54 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+
         args = args.splice(1);
-        switch(cmd) {
+        switch (cmd) {
             // !ping
             case 'ping':
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
                 });
-            break;
+                break;
             case 'quote':
                 bot.sendMessage({
                     to: channelID,
                     message: quotes[quoteIndex]
                 });
-                quoteIndex = quoteIndex+1;
-                if(quoteIndex == quotes.length){
+                quoteIndex = quoteIndex + 1;
+                if (quoteIndex == quotes.length) {
                     quoteIndex = 0;
                 }
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+                break;
+
+            case 'roll':
+                var rollType = args[0];
+                switch (rollType):
+                    case '1d20':
+                        var num = getRNGInteger(1, 20);
+                        var rollStr = "You rolled a " + num.toString + "!";
+                        
+                        //Add the parts that adds the modifiers to the roll
+                        
+                        bot.sendMessage({
+                            to: channelID,
+                            message: rollStr
+                        });
+
+                    default:
+                        bot.sendMessage({
+                            to: channelID,
+                            message: 'Just like target test in Smash 4, that command doesn\'t exist.'
+                        });
+
+
+            default:
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Just like target test in Smash 4, that command doesn\'t exist.'
+                });
+                // Just add any case commands if you want to..
+        }
+    }
 });
