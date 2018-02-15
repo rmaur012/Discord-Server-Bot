8f1c2a1d441280b2a6dce87f5310789e3c59cb1c
rmaur012 <rmaur012@fiu.edu>
2018-02-14 21:08:49 -0500

    Nuke files

--- cmnd_modules/hi_cmnd.js
+++
@@ -1,9 +0,0 @@
-var gf = require('../generalFunc.js');
-
-var intro = "Hello, I am TopTierBOT and I main Bayonetta. :)";
-
-module.exports ={
-    act: function (bot, channelID) {
-        gf.sendMessage(bot, intro, channelID);
-    },
-};
--- cmnd_modules/mv_cmnd.js
+++
@@ -1,7 +0,0 @@
-var gf = require('../generalFunc.js');
-
-module.exports = {
-    act: function (bot, channelID) {
-        gf.sendMessage(bot, "Struz Smash Move Viewer: https://struz.github.io/smash-move-viewer/", channelID);
-    }
-};
