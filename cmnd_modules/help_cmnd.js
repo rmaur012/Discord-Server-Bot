b15de5afe5f139bdb4679102654a215a5acd097a
rmaur012 <rmaur012@fiu.edu>
2018-04-07 14:39:12 -0400

    Update the help command to reflect all the commands that are available

--- cmnd_modules/help_cmnd.js
+++ cmnd_modules/help_cmnd.js
@@ -1,11 +1,18 @@
 var gf = require('../generalFunc.js');
 
 //Each entry is [command, description]
-var commands = [["!hi", "Say hi to TopTierBot"], ["!quote", "Make TTB say a quote from someone"], ["!tourney (ao | vs | ecg)", "Get bracket and stream links for specified tournament"], ["!roll 1d(4|6|8|10|12|20|100) (modifiers spaced out)", "D&D roll for specific number"], ["!kh", "Get link to Kurogane Hammer website"], ["!mv", "Get link to Struz Smash Move Viewer"]];
+var commands = [["!hi", "Say hi to TopTierBot"], 
+                ["!quote", "Make TTB say a quote from someone"], 
+                ["!tourney (ao | vs | ecg)", "Get bracket and stream links for specified tournament"], 
+                ["!r (Any number greater than 1) (modifiers spaced out afterwards)", "D&D roll for specific number"], 
+                ["!poll (Write a yes or on question here)", "A simple poll question will be stated with a thumbs up and down reaction for people to select"],
+                ["!kh", "Get link to Kurogane Hammer website"], 
+                ["!mv", "Get link to Struz Smash Move Viewer"],
+                ["!yt (bsd | msc | jpl)", "Gives you a link to Beefy Smash Doods, My Smash Corner, and Joe's playlist, respectively."]];
 
 module.exports = {
     act: function (msgChannel) {
-        var helpStr = "Command Help Guide\n";
+        var helpStr = "___Command Help Guide___\n";
         for (var i = 0; i < commands.length; i++) {
             helpStr += "**" + commands[i][0] + "** - " + commands[i][1] + "\n";
         }
