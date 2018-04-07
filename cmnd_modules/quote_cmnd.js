2a163195cf9113948e32722bc6aaf1e429b66fa2
rmaur012 <rmaur012@fiu.edu>
2018-04-07 14:09:40 -0400

    Randomize the quote command and add a quote

--- cmnd_modules/quote_cmnd.js
+++ cmnd_modules/quote_cmnd.js
@@ -7,16 +7,11 @@ var quotes = ["\"You need to stop.\" -Joe",
               "\"Joe I'll do a $100 money match once I go 4-2, I swear to god. Eskeetit\" -Yoshi Main",
               "\"Ha ha ha. I SD'ed.\" -Pablo",
               "\"You better not have sweaty hands or you're going to the bathroom and cleaning that shit.\" -Joe",
-              "\"WOOOO, LOOK AT ME! I'M FOX McCLOUD BABY! HONEST!\" -Mida"];
-
-var quoteIndex = 0;
+              "\"WOOOO, LOOK AT ME! I'M FOX McCLOUD BABY! HONEST!\" -Mida",
+              "\"I came to fuck his bitch, but his bitch fucked me.\" -Pablo"];
 
 module.exports = {
     act: function (msgChannel) {
-        gf.sendMessage(quotes[quoteIndex], msgChannel);
-        quoteIndex = quoteIndex + 1;
-        if (quoteIndex == quotes.length) {
-            quoteIndex = 0;
-        }
+        gf.sendMessage(quotes[Math.floor(Math.random() * quotes.length)], msgChannel);
     }
 };
