7bf24fac671fb193607ba70be3aca9f700705929
Ricardo Maury <rmaur012@fiu.edu>
2021-05-27 21:37:45 -0400

    AHHH 2

--- cmnd_modules/smashgg_cmnd.js
+++ cmnd_modules/smashgg_cmnd.js
@@ -119,6 +119,7 @@ function getTop8(args, msgChannel) {
         var i = 0;
         var found = false;
         while (i < resBody.data.tournaments.nodes[0].events[i].name) {
+            console.log("Name: " + resBody.data.tournaments.nodes[0].events[i].name);
             if (resBody.data.tournaments.nodes[0].events[i].name == "Smash Ultimate") {
                 found = true;
                 break;
