f59949d278d2d5259d021863c8eb63c6b1ad7e1d
Ricardo Maury <rmaur012@fiu.edu>
2021-07-16 20:27:46 -0400

    Switching the tourney info from oven to nxt

--- cmnd_modules/tourney_cmnd.js
+++ cmnd_modules/tourney_cmnd.js
@@ -58,9 +58,8 @@ module.exports = {
                 }
                 break;
             case 'nxt':
-            case 'oven':
                 {
-                    displayTourneyInfo(tourneys.oven, msgChannel);
+                    displayTourneyInfo(tourneys.nxt, msgChannel);
                 }
                 break;
             case 'ppg':
