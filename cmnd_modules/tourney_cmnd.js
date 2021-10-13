bd509eee1bd27ca24576ded00613da8597d2cc8e
Ricardo Maury <rmaur012@fiu.edu>
2021-10-12 21:38:32 -0400

    Missed the tourney command

--- cmnd_modules/tourney_cmnd.js
+++ cmnd_modules/tourney_cmnd.js
@@ -64,7 +64,7 @@ module.exports = {
                 break;
             case 'esa':
                 {
-                    displayTourneyInfo(tourneys., msgChannel);
+                    displayTourneyInfo(tourneys.esa, msgChannel);
                 }
                 break;
             default:
